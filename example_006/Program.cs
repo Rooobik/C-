7fd0a8e85d705813af95dd9931bab222e114cc83
Alex Rouchenia <Roubchenia@gmail.com>
2023-02-18 12:10:18 +0300

    гири сравнились

---
+++ example_006/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 1;
+int b = 4;
+int c = 57; 
+int d = 43;
+int e = 13;
+int f = 17;
+int max = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+if(f > max) max = f;
+
+Console.Write("max = ");
+Console.WriteLine(max);
