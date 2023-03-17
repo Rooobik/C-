a2c8200d8f5126d9665f865d104121e1bbcd75dd
Alex Rouchenia <Roubchenia@gmail.com>
2023-03-17 19:38:52 +0300

    Домашнее задание 34,36,38

---
+++ Seminar_5_34/Program.cs
@@ -0,0 +1,41 @@
+﻿int GetNumberConsole(string message)
+{
+    int number = 0;
+    Console.WriteLine(message);
+    string? strValue = Console.ReadLine();
+    if (!int.TryParse(strValue, out number))
+    {
+        Console.WriteLine("Введите цифровое значение");
+        Environment.Exit(-1);
+    }
+    return number;
+}
+
+int[] FillArray(int[] array, int minValue, int maxValue)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int EvenNumberArray(int[] array)
+{
+    int size = array.Length;
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i]%2==0) count++;
+    }
+    return count;
+}
+
+int size = GetNumberConsole("Введите размер массива: ");
+int[] array = new int[size];
+int minValue = 100;
+int maxValue = 999;
+array = FillArray(array, minValue, maxValue);
+
+Console.Write($"В массиве: [{string.Join("; ",array)}]\n количество четных чисел: {EvenNumberArray(array)}\n");
---
+++ Seminar_5_38/Program.cs
@@ -0,0 +1,41 @@
+﻿int GetNumberConsole(string message)
+{
+    int number = 0;
+    Console.WriteLine(message);
+    string? strValue = Console.ReadLine();
+    if (!int.TryParse(strValue, out number))
+    {
+        Console.WriteLine("Введите цифровое значение!");
+        Environment.Exit(-1);
+    }
+    return number;
+}
+
+double[] FillArray(double[] array)
+{
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().NextDouble()*100;
+    }
+    return array;
+}
+
+double DiffMinMax(double[] array)
+{
+    int size = array.Length;
+    double min = array[0];
+    double max = min;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] > max) max = array[i];
+        if (array[i] < min) min = array[i];
+    }
+    return max - min;
+}
+
+int size = GetNumberConsole("Введите размер массива:");
+double[] array = new double[size];
+array = FillArray(array);
+
+Console.WriteLine($"В массиве: [{ string.Join("; ", array)}]\n\nРазница между макс. и мин. значением состовляет: {DiffMinMax(array)}");
